0ce755ba1b2879a69a3e672cd259f8be310f5f7b
Nithyalakshmi <nithyalakshmi223@gmail.com>
2021-12-08 15:20:03 +0530

    Added a new command to fetch resource and account id from PAM360

--- Packs/ManageEngine_PAM360/Integrations/ManageEnginePAM360/ManageEnginePAM360.py
+++ Packs/ManageEngine_PAM360/Integrations/ManageEnginePAM360/ManageEnginePAM360.py
@@ -10,15 +10,44 @@ class Client(BaseClient):
         super().__init__(base_url=server_url, verify=use_ssl, proxy=proxy)
         self._app_token = app_token
 
-    def fetch_password(self, method, resid, accid):
-        URL_SUFFIX = f'/restapi/json/v1/resources/{resid}/accounts/{accid}/password'
+    def fetch_password(self, method, resource_id, account_id, reason, ticket_id):
+        URL_SUFFIX = f'/restapi/json/v1/resources/{resource_id}/accounts/{account_id}/password'
         headers = {
             'APP_AUTHTOKEN': self._app_token,
             'APP_TYPE': '17'
         }
-        return self._http_request(method, URL_SUFFIX, headers=headers)
+        query = {
+            "operation": {
+                "Details": {
+                    "REASON": reason,
+                    "TICKETID": ticket_id
+                }
+            }
+        }
+        params = {
+            'INPUT_DATA': f'{query}'
+        }
+        return self._http_request(method, URL_SUFFIX, headers=headers, params=params)
 
-    def create_resource(self, method, resource_name, account_name, resource_type, resource_url, password, notes, resource_password_policy, account_password_policy):
+    def create_resource(
+        self,
+        method,
+        resource_name,
+        account_name,
+        resource_type,
+        resource_url,
+        password,
+        notes,
+        location,
+        dnsname,
+        department,
+        resource_description,
+        domain_name,
+        resourcegroup_name,
+        owner_name,
+        resource_password_policy,
+        account_password_policy
+    ):
         URL_SUFFIX = '/restapi/json/v1/resources'
         headers = {
             'APP_AUTHTOKEN': self._app_token,
@@ -32,6 +61,13 @@ class Client(BaseClient):
                     "RESOURCETYPE": resource_type,
                     "PASSWORD": password,
                     "NOTES": notes,
+                    "LOCATION": location,
+                    "DNSNAME": dnsname,
+                    "DEPARTMENT": department,
+                    "RESOURCEDESCRIPTION": resource_description,
+                    "DOMAINNAME": domain_name,
+                    "RESOURCEGROUPNAME": resourcegroup_name,
+                    "OWNERNAME": owner_name,
                     "RESOURCEURL": resource_url,
                     "RESOURCEPASSWORDPOLICY": resource_password_policy,
                     "ACCOUNTPASSWORDPOLICY": account_password_policy
@@ -52,10 +88,12 @@ class Client(BaseClient):
         query = {
             "operation": {
                 "Details": {
-                    "ACCOUNTNAME": account_name,
-                    "PASSWORD": password,
-                    "NOTES": notes,
-                    "ACCOUNTPASSWORDPOLICY": account_password_policy
+                    "ACCOUNTLIST": [{
+                        "ACCOUNTNAME": account_name,
+                        "PASSWORD": password,
+                        "NOTES": notes,
+                        "ACCOUNTPASSWORDPOLICY": account_password_policy
+                    }]
                 }
             }
         }
@@ -64,7 +102,20 @@ class Client(BaseClient):
         }
         return self._http_request(method, URL_SUFFIX, headers=headers, data=params)
 
-    def update_resource(self, method, resource_id, resource_name, resource_type, resource_url, password, resource_description, resource_password_policy, location, department):
+    def update_resource(
+        self,
+        method,
+        resource_id,
+        resource_name,
+        resource_type,
+        resource_url,
+        resource_description,
+        resource_password_policy,
+        location,
+        department,
+        dnsname,
+        owner_name
+    ):
         URL_SUFFIX = f'/restapi/json/v1/resources/{resource_id}'
         headers = {
             'APP_AUTHTOKEN': self._app_token,
@@ -75,21 +126,23 @@ class Client(BaseClient):
                 "Details": {
                     "RESOURCENAME": resource_name,
                     "RESOURCETYPE": resource_type,
-                    "PASSWORD": password,
                     "RESOURCEDESCRIPTION": resource_description,
                     "RESOURCEURL": resource_url,
                     "RESOURCEPASSWORDPOLICY": resource_password_policy,
                     "LOCATION": location,
-                    "DEPARTMENT": department
+                    "DEPARTMENT": department,
+                    "DNSNAME": dnsname,
                 }
             }
         }
+        if owner_name != "":
+            query["operation"]["Details"]["OWNERNAME"] = owner_name
         params = {
             'INPUT_DATA': f'{query}'
         }
         return self._http_request(method, URL_SUFFIX, headers=headers, data=params)
 
-    def update_account(self, method, resource_id, account_id, account_name, notes, account_password_policy):
+    def update_account(self, method, resource_id, account_id, account_name, notes, owner_name, account_password_policy):
         URL_SUFFIX = f'/restapi/json/v1/resources/{resource_id}/accounts/{account_id}'
         headers = {
             'APP_AUTHTOKEN': self._app_token,
@@ -100,10 +153,12 @@ class Client(BaseClient):
                 "Details": {
                     "ACCOUNTNAME": account_name,
                     "NOTES": notes,
-                    "ACCOUNTPASSWORDPOLICY": account_password_policy
+                    "ACCOUNTPASSWORDPOLICY": account_password_policy,
                 }
             }
         }
+        if owner_name != "":
+            query["operation"]["Details"]["OWNERNAME"] = owner_name
         params = {
             'INPUT_DATA': f'{query}'
         }
@@ -134,7 +189,7 @@ class Client(BaseClient):
         return self._http_request(method, URL_SUFFIX, headers=headers)
 
     def update_account_password(self, method, resource_id, account_id, new_password, reset_type, reason, ticket_id):
-        URL_SUFFIX = f'/restapi/json/v1/resources/{resource_id}/accounts/{account_id}'
+        URL_SUFFIX = f'/restapi/json/v1/resources/{resource_id}/accounts/{account_id}/password'
         headers = {
             'APP_AUTHTOKEN': self._app_token,
             'APP_TYPE': '17'
@@ -154,13 +209,48 @@ class Client(BaseClient):
         }
         return self._http_request(method, URL_SUFFIX, headers=headers, data=params)
 
+    def fetch_resource_account_id(self, method, resource_name, account_name):
+        URL_SUFFIX = f'/restapi/json/v1/resources/getResourceIdAccountId?RESOURCENAME={resource_name}&ACCOUNTNAME={account_name}'
+        headers = {
+            'APP_AUTHTOKEN': self._app_token,
+            'APP_TYPE': '17'
+        }
+        return self._http_request(method, URL_SUFFIX, headers=headers)
 
-def pam360_fetch_password(client, **args):
-    resid = args.get("resource_id")
-    accid = args.get("account_id")
-    creds_list = client.fetch_password("GET", resid, accid)
-    password = creds_list.get('operation').get('Details').get('PASSWORD')
-    readable_output = f'{password}'
+    def check_connection(self, method):
+        URL_SUFFIX = '/restapi/json/v1/resources/resourcetypes'
+        headers = {
+            'APP_AUTHTOKEN': self._app_token,
+            'APP_TYPE': '17'
+        }
+        return self._http_request(method, URL_SUFFIX, headers=headers)
+
+
+def test_module(
+        client: Client,
+):
+    URL_SUFFIX = '/restapi/json/v1/resources/resourcetypes'
+    headers = {
+        'APP_AUTHTOKEN': client._app_token,
+        'APP_TYPE': '17'
+    }
+    r = requests.request("GET", client._base_url + URL_SUFFIX, headers=headers, verify=client._verify)
+    if r.status_code != 200:
+        return 'Failed to connect to server'
+    else:
+        return 'ok'
+
+
+def pam360_fetch_password(
+        client: Client,
+        resource_id: str = "",
+        account_id: str = "",
+        reason: str = "",
+        ticket_id: str = ""
+):
+    creds_list = client.fetch_password("GET", resource_id, account_id, reason, ticket_id)
+    '''password = creds_list.get('operation').get('Details').get('PASSWORD')'''
+    readable_output = f'{creds_list}'
     results = CommandResults(
         outputs=creds_list,
         raw_response=creds_list,
@@ -171,16 +261,28 @@ def pam360_fetch_password(client, **args):
     return results
 
 
-def pam360_create_resource(client, **args):
-    resource_name = args.get("resource_name")
-    account_name = args.get("account_name")
-    resource_type = args.get("resource_type")
-    resource_url = args.get("resource_url")
-    password = args.get("password")
-    notes = args.get("notes")
-    resource_password_policy = args.get("resource_password_policy")
-    account_password_policy = args.get("account_password_policy")
-    create_resource = client.create_resource("POST", resource_name, account_name, resource_type, resource_url, password, notes, resource_password_policy, account_password_policy)
+def pam360_create_resource(
+        client: Client,
+        resource_name: str = "",
+        account_name: str = "",
+        resource_type: str = "",
+        resource_url: str = "",
+        password: str = "",
+        notes: str = "",
+        location: str = "",
+        dnsname: str = "",
+        department: str = "",
+        resource_description: str = "",
+        domain_name: str = "",
+        resourcegroup_name: str = "",
+        owner_name: str = "",
+        resource_password_policy: str = "",
+        account_password_policy: str = ""
+):
+    create_resource = client.create_resource("POST", resource_name, account_name, resource_type, resource_url,
+                                             password, notes, location, dnsname, department, resource_description,
+                                             domain_name, resourcegroup_name, owner_name, resource_password_policy,
+                                             account_password_policy)
     readable_output = f'{create_resource}'
     results = CommandResults(
         outputs=create_resource,
@@ -192,12 +294,14 @@ def pam360_create_resource(client, **args):
     return results
 
 
-def pam360_create_account(client, **args):
-    resource_id = args.get("resource_id")
-    account_name = args.get("account_name")
-    password = args.get("password")
-    notes = args.get("notes")
-    account_password_policy = args.get("account_password_policy")
+def pam360_create_account(
+        client: Client,
+        resource_id: str = "",
+        account_name: str = "",
+        password: str = "",
+        notes: str = "",
+        account_password_policy: str = ""
+):
     create_account = client.create_account("POST", resource_id, account_name, password, notes, account_password_policy)
     readable_output = f'{create_account}'
     results = CommandResults(
@@ -210,17 +314,22 @@ def pam360_create_account(client, **args):
     return results
 
 
-def pam360_update_resource(client, **args):
-    resource_id = args.get("resource_id")
-    resource_name = args.get("resource_name")
-    resource_type = args.get("resource_type")
-    resource_url = args.get("resource_url")
-    password = args.get("password")
-    resource_description = args.get("resource_description")
-    resource_password_policy = args.get("resource_password_policy")
-    location = args.get("location")
-    department = args.get("department")
-    update_resource = client.update_resource("PUT", resource_id, resource_name, resource_type, resource_url, password, resource_description, resource_password_policy, location, department)
+def pam360_update_resource(
+        client: Client,
+        resource_id: str = "",
+        resource_name: str = "",
+        resource_type: str = "",
+        resource_url: str = "",
+        resource_description: str = "",
+        resource_password_policy: str = "",
+        location: str = "",
+        department: str = "",
+        dnsname: str = "",
+        owner_name: str = ""
+):
+    update_resource = client.update_resource("PUT", resource_id, resource_name, resource_type, resource_url,
+                                             resource_description, resource_password_policy, location, department,
+                                             dnsname, owner_name)
     readable_output = f'{update_resource}'
     results = CommandResults(
         outputs=update_resource,
@@ -232,13 +341,17 @@ def pam360_update_resource(client, **args):
     return results
 
 
-def pam360_update_account(client, **args):
-    resource_id = args.get("resource_id")
-    account_id = args.get("account_id")
-    account_name = args.get("account_name")
-    notes = args.get("notes")
-    account_password_policy = args.get("account_password_policy")
-    update_account = client.update_account("PUT", resource_id, account_id, account_name, notes, account_password_policy)
+def pam360_update_account(
+        client: Client,
+        resource_id: str = "",
+        account_id: str = "",
+        account_name: str = "",
+        notes: str = "",
+        owner_name: str = "",
+        account_password_policy: str = ""
+):
+    update_account = client.update_account("PUT", resource_id, account_id, account_name, notes, owner_name,
+                                           account_password_policy)
     readable_output = f'{update_account}'
     results = CommandResults(
         outputs=update_account,
@@ -250,9 +363,11 @@ def pam360_update_account(client, **args):
     return results
 
 
-def pam360_fetch_account_details(client, **args):
-    resource_id = args.get("resource_id")
-    account_id = args.get("account_id")
+def pam360_fetch_account_details(
+        client: Client,
+        resource_id: str = "",
+        account_id: str = ""
+):
     account_details = client.fetch_account_details("GET", resource_id, account_id)
     readable_output = f'{account_details}'
     results = CommandResults(
@@ -278,8 +393,10 @@ def pam360_list_resources(client, **args):
     return results
 
 
-def pam360_list_accounts(client, **args):
-    resource_id = args.get("resource_id")
+def pam360_list_accounts(
+        client: Client,
+        resource_id: str = ""
+):
     account_list = client.fetch_accounts("GET", resource_id)
     readable_output = f'{account_list}'
     results = CommandResults(
@@ -292,13 +409,15 @@ def pam360_list_accounts(client, **args):
     return results
 
 
-def pam360_update_account_password(client, **args):
-    resource_id = args.get("resource_id")
-    account_id = args.get("account_id")
-    new_password = args.get("new_password")
-    reset_type = args.get("reset_type")
-    reason = args.get("reason")
-    ticket_id = args.get("ticket_id")
+def pam360_update_account_password(
+        client: Client,
+        resource_id: str = "",
+        account_id: str = "",
+        new_password: str = "",
+        reset_type: str = "",
+        reason: str = "",
+        ticket_id: str = ""
+):
     update_password = client.update_account_password("PUT", resource_id, account_id, new_password, reset_type, reason, ticket_id)
     readable_output = f'{update_password}'
     results = CommandResults(
@@ -311,11 +430,28 @@ def pam360_update_account_password(client, **args):
     return results
 
 
+def pam360_fetch_resource_account_id(
+        client: Client,
+        resource_name: str = "",
+        account_name: str = ""
+):
+    fetch_id = client.fetch_resource_account_id("GET", resource_name, account_name)
+    readable_output = f'{fetch_id}'
+    results = CommandResults(
+        outputs=fetch_id,
+        raw_response=fetch_id,
+        outputs_prefix='PAM360.Resource',
+        outputs_key_field='message',
+        readable_output=readable_output,
+    )
+    return results
+
+
 def main():
 
     params = demisto.params()
-    url = params.get('URL')
-    app_token = params.get('APP_TOKEN')
+    url = params.get('url')
+    app_token = params.get('appToken')
     use_ssl = not params.get('insecure', False)
     proxy = params.get('proxy', False)
     try:
@@ -323,6 +459,7 @@ def main():
         command = demisto.command()
         demisto.debug(f'Command being called in ManageEngine PAM360 is: {command}')
         commands = {
+            'test-module': test_module,
             'pam360-fetch-password': pam360_fetch_password,
             'pam360-create-resource': pam360_create_resource,
             'pam360-create-account': pam360_create_account,
@@ -332,6 +469,7 @@ def main():
             'pam360-list-all-resources': pam360_list_resources,
             'pam360-list-all-accounts': pam360_list_accounts,
             'pam360-update-account-password': pam360_update_account_password,
+            'pam360-fetch-resource-account-id': pam360_fetch_resource_account_id,
         }
         if command in commands:
             return_results(commands[command](client, **demisto.args()))
--- Packs/ManageEngine_PAM360/Integrations/ManageEnginePAM360/README.md
+++ Packs/ManageEngine_PAM360/Integrations/ManageEnginePAM360/README.md
@@ -17,3 +17,48 @@ Use the ManageEngine PAM360 to manage passwords, resources and accounts from Cor
 ## Commands
 You can execute these commands from the Cortex XSOAR CLI, as part of an automation, or in a playbook.
 After you successfully execute a command, a DBot message appears in the War Room with the command details.
+### pam360-fetch-password
+***
+Fetch password from PAM360 vault.
+
+
+#### Base Command
+
+`pam360-fetch-password`
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| resource_id | ID of the resource. | Required | 
+| account_id | ID of the account | Required | 
+| reason | Reason to fetch password | Optional | 
+| ticket_id | To fetch ticket ID when the ticketing system integration is enabled. | Optional | 
+
+
+#### Context Output
+
+| **Path** | **Type** | **Description** |
+| --- | --- | --- |
+| PAM360.Account.operation.Details.PASSWORD | String | Password of the account stored in PAM360 |
+| PAM360.Account.operation.result.status | String | Status of the API call |
+| PAM360.Account.operation.result.message | String | Message of the API call |
+
+
+#### Command Example
+```!pam360-fetch-password resource_id=1 account_id=1 reason="Need the password to Login Windows Server" ticket_id=7```
+
+#### Context Example
+```
+{
+   "operation":{
+      "result":{
+         "message":"Password fetched successfully",
+         "status":"Success"
+      },
+      "Details":{
+         "PASSWORD":"A1@8ZnQx)mh&="
+      },
+      "name":"GET PASSWORD"
+   }
+}
+```
